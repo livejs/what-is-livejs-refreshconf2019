f5ddb2496f8a6e8eea0838b2623d164b7808fb52
Tim Pietrusky <timpietrusky@gmail.com>
2019-10-26 23:27:35 +0200

    feat(slides): Initial commit

---
+++ README.md
@@ -0,0 +1,12 @@
+<h1>What is {live:js}?</h1>
+
+- [Slides](#slides)
+
+---
+
+## Slides
+
+* Install dependencies with `npm install`
+* Start the server with `npm start`
+* Open the slides on http://localhost:8083
+
---
+++ src/slides/cover.js
@@ -0,0 +1,43 @@
+import Config from './config'
+import React from 'react'
+import uuid from 'uuid/v4'
+import Fragment from '@dekk/fragment'
+import {Text, Title, Subtitle, Uppercase, Bold, Center, Code} from '@dekk/text'
+import {default as MaskedImage, FitImage} from '@dekk/image'
+import Notes from '@dekk/speaker-notes'
+import {Main} from '@dekk/master-slides'
+import {Plugins} from '@dekk/deck'
+import {RainbowText} from '../components'
+import {css} from 'styled-components'
+import {Grid, Half, HalfVertical} from '../masters'
+import {ImportantLink} from '../components/links'
+import {topCenter, LargeImage} from '../components/images'
+import {fade, flip, cube} from '@dekk/animation'
+
+const {Slide, A} = Main
+
+const notes = (
+  <Notes>
+    <h3 />
+    <p />
+  </Notes>
+)
+
+export default (
+  <Slide key={uuid()}>
+    <Plugins.Data luminave={['']} />
+    {notes}
+
+    <A>
+      <Title>
+        What is&nbsp;
+        <RainbowText
+          style={{filter: css`invert(0%)`}}
+          time={30}
+          text="{live:js}"
+        />
+        ?
+      </Title>
+    </A>
+  </Slide>
+)
